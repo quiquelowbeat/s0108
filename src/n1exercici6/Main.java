ccc7904e7b4e404b374273cfdbd70a1f37a8e86e
Quique Sánchez Suárez <kikeebcn@gmail.com>
2022-02-22 00:21:04 +0100

    n1 - exercici 6 acabado

---
+++ src/n1exercici6/Main.java
@@ -0,0 +1,16 @@
+package n1exercici6;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<String> listMonths = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+		
+		listMonths.forEach(System.out::println);
+	
+	}
+
+}
