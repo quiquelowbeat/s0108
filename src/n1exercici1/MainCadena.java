9084e0d65cf024e0b507083aaabb771575088ea4
Quique Sánchez Suárez <kikeebcn@gmail.com>
2022-02-22 00:05:46 +0100

    n1 - exercici 1 acabado

---
+++ src/n1exercici1/MainCadena.java
@@ -0,0 +1,25 @@
+package n1exercici1;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainCadena {
+
+	public static void main(String[] args) {
+		
+		List<String> arrayCadena = Arrays.asList("Ara", "Ana", "Josep", "Pedro", "alf", "Quique", "Ari");
+		
+ 		filteredList(arrayCadena).forEach(System.out::println);
+		
+	}
+	
+	public static List<String> filteredList(List<String> list) {
+		
+		return list.stream()
+				.filter(str -> ((String) str).startsWith("A"))
+     			.filter(str -> ((String) str).length() == 3)
+				.toList();
+		
+	}
+}
