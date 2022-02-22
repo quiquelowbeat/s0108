81baecd4f74310aacf05eaaa381651464cf1240c
Quique Sánchez Suárez <kikeebcn@gmail.com>
2022-02-22 10:23:14 +0100

    n1f2 - acabado

---
+++ src/n1f2exercici1/Main.java
@@ -0,0 +1,13 @@
+package n1f2exercici1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PiValue pi1 = () -> 3.1415;
+		
+		System.out.println(pi1.getPiValue());
+		
+	}
+
+}
---
+++ src/n1f2exercici1/PiValue.java
@@ -0,0 +1,8 @@
+package n1f2exercici1;
+
+@FunctionalInterface
+public interface PiValue {
+	
+	double getPiValue();
+
+}
